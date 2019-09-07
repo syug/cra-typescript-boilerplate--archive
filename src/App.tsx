4961d8d399884ac08e75f180b805dd1f0eefa1dc
Syugo Saito <syug@izm.org.uk>
2019-09-07 18:27:21 +0900

    added - eslint-config-airbnb

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import styled from 'styled-components';
+import logo from './logo.svg';
 
 const StyledApp = styled.div`
   text-align: center;
