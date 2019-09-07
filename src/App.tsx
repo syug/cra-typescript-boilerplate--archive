a2011a8ccac0c54140d57ec4938082c1ac1e008f
Syugo Saito <syug@izm.org.uk>
2019-09-07 18:07:57 +0900

    added - config file for prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import logo from "./logo.svg";
-import styled from "styled-components";
+import React from 'react';
+import logo from './logo.svg';
+import styled from 'styled-components';
 
 const StyledApp = styled.div`
   text-align: center;
